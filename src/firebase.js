163e65a9c0f504e3eb0ed3e469a23d40774563f7
solomontaiwo <zel9697@gmail.com>
2024-08-22 23:55:30 +0200

    Env example

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCEMCP41iugDnlcPgnj377Fgb_VqRS2TlM",
-  authDomain: "soldi-sotto.firebaseapp.com",
-  projectId: "soldi-sotto",
-  storageBucket: "soldi-sotto.appspot.com",
-  messagingSenderId: "885892536806",
-  appId: "1:885892536806:web:5867dde9e0baa9bf4ede27",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Inizializza Firebase
