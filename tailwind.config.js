315b56f6a4699199852816374cd07e394c46b539
solomontaiwo <zel9697@gmail.com>
2024-11-04 14:37:52 +0100

    Update: installed shadcn-ui

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@shadcn/ui/**/*.js",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
