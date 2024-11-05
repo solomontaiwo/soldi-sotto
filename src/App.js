0ca83d7d9efcae0710409519c0354227194d0a8a
solomontaiwo <zel9697@gmail.com>
2024-11-05 11:22:08 +0100

    Fix: potential fix for infinite load in pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
@@ -9,29 +8,18 @@ import Stats from "./components/Stats/Stats";
 import Navbar from "./components/Navbar/Navbar";
 import "./App.css";
 
-function AnimatedRoutes() {
-  const location = useLocation();
-
-  return (
-    <TransitionGroup>
-      <CSSTransition key={location.key} timeout={300} classNames="fade">
-        <Routes location={location}>
-          <Route path="/soldi-sotto" element={<Home />} />
-          <Route path="/soldi-sotto/login" element={<Login />} />
-          <Route path="/soldi-sotto/register" element={<Register />} />
-          <Route path="/soldi-sotto/transactions" element={<Transactions />} />
-          <Route path="/soldi-sotto/stats" element={<Stats />} />
-        </Routes>
-      </CSSTransition>
-    </TransitionGroup>
-  );
-}
-
 function App() {
   return (
     <Router>
       <Navbar />
-      <AnimatedRoutes />
+      <Routes>
+        <Route path="/soldi-sotto" element={<Home />} />
+        <Route path="/soldi-sotto/login" element={<Login />} />
+        <Route path="/soldi-sotto/register" element={<Register />} />
+        <Route path="/soldi-sotto/transactions" element={<Transactions />} />
+        <Route path="/soldi-sotto/stats" element={<Stats />} />{" "}
+      </Routes>
+
     </Router>
   );
 }
