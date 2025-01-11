b381440bb30659270a74d82914dac2ff5ddb9671
Solomon Taiwo <zel9697@gmail.com>
2025-01-11 22:45:54 +0100

    update: removed unused import

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { App as AntdApp } from "antd"; // Importa il wrapper App di Ant Design
 import "./main.css";
 import { AuthProvider } from "./components/Auth/AuthProvider";
 import { TransactionProvider } from "./components/Transaction/TransactionProvider"; // Importa il provider
