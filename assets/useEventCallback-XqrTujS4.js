58c3158f8b4259b43b902946639c96cae6dcf321
Solomon Taiwo <zel9697@gmail.com>
2025-07-06 21:32:21 +0200

    Updates

--- assets/useEventCallback-XqrTujS4.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-DCNmr1x3.js";function u(t){const r=e.useRef(t);return e.useEffect(()=>{r.current=t},[t]),r}function f(t){const r=u(t);return e.useCallback(function(...n){return r.current&&r.current(...n)},[r])}export{f as u};
