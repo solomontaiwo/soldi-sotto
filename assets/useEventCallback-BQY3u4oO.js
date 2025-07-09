8320f9c431def4baaf93ba450958ba722a637e17
Solomon Taiwo <zel9697@gmail.com>
2025-07-09 12:59:14 +0200

    Updates

--- assets/useEventCallback-BQY3u4oO.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-DhPDyLgL.js";function u(t){const r=e.useRef(t);return e.useEffect(()=>{r.current=t},[t]),r}function f(t){const r=u(t);return e.useCallback(function(...n){return r.current&&r.current(...n)},[r])}export{f as u};
