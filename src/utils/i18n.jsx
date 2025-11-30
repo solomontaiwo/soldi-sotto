5466c8e8de956864b10b73fbdf6ade360241b596
Solomon Taiwo <zel9697@gmail.com>
2025-11-30 19:29:59 +0100

    Chore: added supported languages to i18n

--- src/utils/i18n.jsx
+++ src/utils/i18n.jsx
@@ -1,4 +1,3 @@
-// i18n.js - i18next configuration for React internationalization
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
@@ -6,22 +5,19 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import translationEN from '../locales/en/translation.json';
 import translationIT from '../locales/it/translation.json';
 
-// The translations
 const resources = {
   en: { translation: translationEN },
   it: { translation: translationIT },
 };
 
-// Initialize i18next
-// This will detect the user's language and fallback to Italian if not found
-// You can add more options as needed
-
+// This will detect the user's language and fallback to english if not found
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources,
     fallbackLng: 'en',
+    supportedLngs: ['en', 'it'],
     interpolation: {
       escapeValue: false, // React already escapes by default
     },
